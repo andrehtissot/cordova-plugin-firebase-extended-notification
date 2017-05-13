3ca6e719dcaacccf3702002ca72b789ca4e4263a
André Tissot <andrehtissot@gmail.com>
2017-05-13 20:28:30 -0300

    Update beforePrepareBuild.js
    Fixed for old node.js

--- scripts/beforePrepareBuild.js
+++ scripts/beforePrepareBuild.js
@@ -2,13 +2,13 @@ module.exports = function(context) {
   var fs = require('fs'), filename = context.opts.projectRoot+"/platforms/android/AndroidManifest.xml";
   fs.readFile(filename, 'utf8', function(err, sourceText) {
     if (err) return; // On error, fail silently.
-    let duplicatedString =
+    var duplicatedString =
 '        <activity android:exported="true" android:launchMode="singleTop" android:name="com.andretissot.firebaseextendednotification.NotificationActivity" android:screenOrientation="portrait">\n'+
 '            <intent-filter>\n'+
 '                <category android:name="android.intent.category.DEFAULT" />\n'+
 '            </intent-filter>\n'+
 '        </activity>\n';
-    let text = sourceText.split(duplicatedString);
+    var text = sourceText.split(duplicatedString);
     if(text.length === 1) {
       return; // Duplicate not found, nothing to do here.
     }
