e71dadaf9d08fd8b86d5d1c5bf90d44603c1fc23
André Augusto Tissot <andre.tissot@mytheresa.com>
2019-08-26 21:40:26 +0200

    Upgraded cordova-plugin-fcm-with-dependecy-updated dependency to version 3.0.0

--- scripts/beforePrepareBuild.js
+++ scripts/beforePrepareBuild.js
@@ -1,79 +1,50 @@
-var fs = require('fs');
+var fs = require("fs");
 
-var legacyPrepareBuild = function(legacyFilePath) {
-  fs.readFile(legacyFilePath, 'utf8', function(err, sourceText) {
-    if (err) return; // On error, fail silently.
-    var stringToInsert1 = '\
-        <activity android:exported="true" android:launchMode="singleTop" android:name="com.andretissot.firebaseextendednotification.NotificationActivity">\n\
-            <intent-filter>\n\
-                <category android:name="android.intent.category.DEFAULT" />\n\
-            </intent-filter>\n\
-        </activity>\n',
-      stringToInsert2 = '\
-        <service android:name="com.andretissot.firebaseextendednotification.MessagingService">\n\
-            <intent-filter>\n\
-                <action android:name="com.google.firebase.MESSAGING_EVENT" />\n\
-            </intent-filter>\n\
-        </service>\n';
-    var text = sourceText, textLenght = 0;
-    while(text.length != textLenght) {
-      textLenght = text.length;
-      text = text.replace(stringToInsert1, '');
-    }
-    textLenght = 0;
-    while(text.length != textLenght) {
-      textLenght = text.length;
-      text = text.replace(stringToInsert2, '');
-    }
-    var indexFound = text.lastIndexOf('        <activity', text.indexOf('FCMPluginActivity'));
-    if(indexFound !== -1)
-      text = text.substring(0, indexFound)+stringToInsert1+stringToInsert2+text.substring(indexFound);
-    fs.writeFile(legacyFilePath, text, function(err) {
-      if (err) return; // On error, fail silently.
-    });
-  });
-}
+var removeText = function(sourceText, stringToRemove) {
+  var text = sourceText;
+  var textLenght = 0;
+  while (text.length != textLenght) {
+    textLenght = text.length;
+    text = text.replace(stringToRemove, "");
+  }
+  return text;
+};
 
 var prepareBuild = function(filePath) {
-  fs.readFile(filePath, 'utf8', function(err, sourceText) {
+  fs.readFile(filePath, "utf8", function(err, sourceText) {
     if (err) return; // On error, fail silently.
-    var stringToInsert1 = '\
+    var text = removeText(
+      sourceText,
+      '\
         <activity android:exported="true" android:launchMode="singleTop" android:name="com.gae.scaffolder.plugin.FCMPluginActivity">\n\
             <intent-filter>\n\
                 <action android:name="FCM_PLUGIN_ACTIVITY" />\n\
                 <category android:name="android.intent.category.DEFAULT" />\n\
             </intent-filter>\n\
-        </activity>\n';
-    var stringToInsert2 = '\
-        <service android:name="com.gae.scaffolder.plugin.MyFirebaseMessagingService">\n\
+        </activity>\n'
+    );
+    text = removeText(
+      text,
+      '\
+        <service android:name="com.gae.scaffolder.plugin.MyFirebaseMessagingService" android:stopWithTask="false">\n\
             <intent-filter>\n\
                 <action android:name="com.google.firebase.MESSAGING_EVENT" />\n\
             </intent-filter>\n\
-        </service>\n';
-    var text = sourceText, textLenght = 0;
-    while(text.length != textLenght) {
-      textLenght = text.length;
-      text = text.replace(stringToInsert1, '');
-    }
-    textLenght = 0;
-    while(text.length != textLenght) {
-      textLenght = text.length;
-      text = text.replace(stringToInsert2, '');
-    }
+        </service>\n'
+    );
     fs.writeFile(filePath, text, function(err) {
       if (err) return; // On error, fail silently.
     });
   });
-}
+};
 
 module.exports = function(context) {
-  var legacyFilePath = context.opts.projectRoot+"/platforms/android/AndroidManifest.xml",
-    filePath = context.opts.projectRoot+"/platforms/android/app/src/main/AndroidManifest.xml";
-  if (fs.existsSync(legacyFilePath)) {
-    legacyPrepareBuild(legacyFilePath);
-  } else if(fs.existsSync(filePath)) {
+  var filePath =
+    context.opts.projectRoot +
+    "/platforms/android/app/src/main/AndroidManifest.xml";
+  if (fs.existsSync(filePath)) {
     prepareBuild(filePath);
   } else {
-    console.error("AndroidManifest.xml file not found!")
+    throw new Error("AndroidManifest.xml file not found!");
   }
-}
+};
